f0bb4787580899696af3cc1fbd6a04813100e1c1
玩火 <niltok@163.com>
2023-02-01 22:28:52 +0800

    finish moving logic

--- server/src/main/java/ikuyo/server/behaviors/ControlMovingBehavior.java
+++ server/src/main/java/ikuyo/server/behaviors/ControlMovingBehavior.java
@@ -3,13 +3,29 @@ package ikuyo.server.behaviors;
 public class ControlMovingBehavior extends AbstractBehavior {
     @Override
     public void update() {
-        var speed = 5; // TODO: moving logic
+        var speed = 5;
         context.userKeyInputs().forEach((id, input) -> {
             var pos = context.star().starInfo().starUsers.get(id);
-            if (input.up > 0) pos.y -= speed;
-            if (input.down > 0) pos.y += speed;
-            if (input.left > 0) pos.x -= speed;
-            if (input.right > 0) pos.x += speed;
+            if (!pos.online) return;
+            var z = Math.hypot(pos.x, pos.y);
+            var dx = speed * (z == 0 ? 0 : pos.x / z);
+            var dy = speed * (z == 0 ? 1 : pos.y / z);
+            if (input.up > 0) {
+                pos.x += dx;
+                pos.y += dy;
+            }
+            if (input.down > 0) {
+                pos.x -= dx;
+                pos.y -= dy;
+            }
+            if (input.left > 0) {
+                pos.x += dy;
+                pos.y -= dx;
+            }
+            if (input.right > 0) {
+                pos.x -= dy;
+                pos.y += dx;
+            }
         });
     }
 }
