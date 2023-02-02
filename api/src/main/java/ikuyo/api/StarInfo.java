e1fd9eb3acc7770337ee95d975595db1b470baab
Qianxuan <2392009257@qq.com>
2023-02-03 01:51:12 +0800

    SratInfo&&tools

--- api/src/main/java/ikuyo/api/StarInfo.java
+++ api/src/main/java/ikuyo/api/StarInfo.java
@@ -7,10 +7,15 @@ import io.vertx.core.json.JsonObject;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class StarInfo {
     public Block[] blocks;
     public Map<Integer, StarUserInfo> starUsers;
+///   层级最大值
+    public int maxtier = 10;
+///   层级最小值
+    public int mintier = 5;
 
     public JsonObject toJson() {
         return JsonObject.mapFrom(this);
@@ -24,14 +29,30 @@ public class StarInfo {
     public Buffer toBuffer() {
         return DataStatic.gzipEncode(toJson().toBuffer());
     }
-
     public static StarInfo gen(int seed) {
         var info = new StarInfo();
-        info.blocks = new Block[100];
-        for (var i = 0; i < 100; i++) {
-            info.blocks[i] = new Block.Normal();
-            info.blocks[i].type = 0;
+
+        ArrayList<Block> block = new ArrayList<>();
+        Random random = new Random(seed);
+
+        int tiernum = (int)(random.nextDouble()*(info.maxtier-info.mintier)) + info.mintier;
+//        tiernum = 200; info.mintier = 150;
+//        System.out.printf("max:%d\tmin:%d%n", info.maxtier, info.mintier);
+        int blocknum = tiernum*(tiernum+1)*3 + info.mintier*(info.mintier+1)*3;
+//        System.out.println(blocknum);
+
+        for (var i = 0; i < blocknum; i++) {
+            Block newblock = new Block.Normal();
+            newblock.type = 0;
+            newblock.isVisible = true;
+            newblock.isDestructible = true;
+            newblock.isInteractive = true;
+            block.add(newblock);
         }
+        info.blocks = block.toArray(new Block[0]);
+
+//        System.out.println(info.blocks.length);
+
         info.starUsers = new HashMap<>();
         return info;
     }
@@ -58,4 +79,44 @@ public class StarInfo {
             online = true;
         }
     }
+
+    public static void main(String[] args) {
+        StarInfo.gen(0);
+//        Position pos = StarInfo.posOf(43);
+//        System.out.print("angle1:\t");
+//        System.out.println(StarInfo.angleOf(43));
+//        System.out.print("tier:\t");
+//        System.out.println(StarInfo.tierOf(43));
+//        System.out.printf(String.format("%g, %g, \nangle2:\t%g", pos.x, pos.y, Math.atan(pos.y/pos.x)));
+    }
+
+    public static int realINdexOf(int index, int mintier) {
+        return 3*mintier*(mintier+1) + index + 1;
+    }
+
+    public static int tierOf(int realindex) {
+        return (int)(0.5 + Math.pow((12*realindex+9), 1.0/2)/6.0);
+    }
+
+    public static Position posOf(int realindex) {
+        Position pos = new Position();
+        int tier = tierOf(realindex);
+        int reltindex = realindex - 3*tier*(tier-1) - 1;
+        double i = (reltindex%tier)/(double)tier;
+        double x = 1 - i * Math.cos(Math.PI/3.0);
+        double y = i * Math.sin(Math.PI/3.0);
+        double l = Math.hypot(x,y);
+        double angle = Math.atan(y/x) + Math.PI*(reltindex/tier)/3.0;
+        pos.x = Math.cos(angle) * l * tier;
+        pos.y = Math.sin(angle) * l * tier;
+        return pos;
+    }
+//    public static double angleOf(int realindex) {
+//        int tier = tierOf(realindex);
+//        int reltindex = realindex - 3*tier*(tier-1) - 1;
+//        double i = (reltindex%tier)/(double)tier;
+//        double x = 1 - i * Math.cos(Math.PI/3.0);
+//        double y = i * Math.sin(Math.PI/3.0);
+//        return Math.atan(y/x) + Math.PI*(int)(reltindex/tier)/3.0;
+//    }
 }
--- server/src/main/java/ikuyo/server/renderers/DrawableRenderer.java
+++ server/src/main/java/ikuyo/server/renderers/DrawableRenderer.java
@@ -1,7 +1,9 @@
 package ikuyo.server.renderers;
 
 import ikuyo.api.Drawable;
+import ikuyo.api.Position;
 import ikuyo.api.Star;
+import ikuyo.api.StarInfo;
 import ikuyo.server.api.Renderer;
 import io.vertx.core.json.JsonObject;
 
@@ -13,12 +15,17 @@ public class DrawableRenderer implements Renderer {
     @Override
     public JsonObject render(Star star) {
         var drawables = new ArrayList<Drawable>();
+        int rindex = StarInfo.realINdexOf(0, star.starInfo().mintier);
         for (var i = 0; i < star.starInfo().blocks.length; i++) {
             var block = star.starInfo().blocks[i];
             var d = new Drawable.Sprite();
+            Position pos = StarInfo.posOf(rindex);
+            d.x = pos.x;
+            d.y = pos.y;
             d.bundle = "blocks";
             d.asset = String.valueOf(block.type);
             drawables.add(d);
+            rindex++;
         }
         return new JsonObject(drawables.stream().map(JsonObject::mapFrom).collect(Collectors.toMap(
                 json -> String.valueOf(json.hashCode()),
