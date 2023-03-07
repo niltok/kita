203b1a794aa465fd3188522938e01c2ab67368ac
玩火 <niltok@163.com>
2023-03-07 22:36:26 +0800

    add window rect filter

--- client/src/worker/preprocessor.worker.ts
+++ client/src/worker/preprocessor.worker.ts
@@ -41,9 +41,15 @@ onmessage = async (e: MessageEvent<StateEvent>) => {
 
 setInterval( () => {
     const res = new Set<string>()
-    const r = Math.hypot(state.windowSize.height, state.windowSize.width) * 0.6
+    const wh = state.windowSize.height * 0.51
+    const ww = state.windowSize.width * 0.51
+    const cx = state.camera.x, cy = state.camera.y
+    const cr = Math.hypot(cx, cy)
+    const nx = cx / cr, ny = cy / cr
     drawables.forEach((d, k) => {
-        if (Math.hypot(d.x - state.camera.x, d.y - state.camera.y) < r) res.add(k)
+        const x_ = d.x - cx, y_ = d.y - cy
+        const dx = - x_ * ny + y_ * nx, dy = - x_ * nx - y_ * ny
+        if (Math.abs(dx) < ww && Math.abs(dy) < wh) res.add(k)
     })
     const data = ({
         add: subtractSet(res, prev),
