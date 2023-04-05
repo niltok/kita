1c328221f6065f58c6cf5744499097e701d666d9
Qianxuan <2392009257@qq.com>
2023-04-05 16:35:24 +0800

    _utilsPatch

--- api/src/test/java/ikuyo/utils/StarUtilsTest.java
+++ api/src/test/java/ikuyo/utils/StarUtilsTest.java
@@ -11,19 +11,17 @@ import java.util.List;
 public class StarUtilsTest {
     @Test
     public void printBlock() {
-        System.out.println(StarUtils.printBlock(6771));
+        System.out.println(StarUtils.printBlock(134727));
     }
     @Test
     public void Teat_area() {
         int num = 0, error = 0;
         int testArea = StarUtils.areaNum;
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 19; i++) {
             List<Integer> list = StarUtils.getBlocksAt(i);
             for (var index: list) {
                 num++;
                 int reaIndex = StarUtils.realIndexOf(index);
-//                System.out.println(i + "," + StarUtils.getAreaOf(reaIndex) + ";index: " + reaIndex);
-
                 if (StarUtils.getAreaOf(reaIndex) != i) {
                     error++;
                     System.out.println(i + "," + StarUtils.getAreaOf(reaIndex) + ";realIndex: " + reaIndex);
@@ -33,14 +31,13 @@ public class StarUtilsTest {
             }
         }
 
-        System.out.println(num + "," + error);
-
+        System.out.printf("[totalNumber]: %d, [ErrorNumber]: %d%n", num, error);
     }
 
     @Test
     public void Teat_singleArea() {
         List<Integer> list = StarUtils.areasAround(292.2, -506.2, 20);
-        list = StarUtils.getBlocksAt(2068);
+        list = StarUtils.getBlocksAt(16);
         System.out.println(StarUtils.getAreaOf(StarUtils.realIndexOf(292.2, -506.2)));
 
     }
@@ -53,16 +50,28 @@ public class StarUtilsTest {
             Position pos = StarUtils.positionOf(index);
             for (int i = 0; i < 6; i++) {
                 var v = new Vector2(pos.x, pos.y)
-                        .add(new Vector2(0.499, 0).rotate(i * Math.PI / 3));
+                        .add(new Vector2(0, 0).rotate(i * Math.PI / 3));
                 if (index != StarUtils.realIndexOf(v.x, v.y)) {
                     error++;
-//                    int tier = StarInfo.tierOf(index);
+                    int tier = StarUtils.tierOf(index);
+//                    int t = StarUtils.tierOf(StarUtils.realIndexOf(pos.x, pos.y));
+//                    System.out.println(tier + ", " + t);
 //                    System.out.printf("[index]: %d, [cul]: %d, [tier]: %d, [percent]: %f%n",
-//                            index, StarInfo.realIndexOf(pos.x, pos.y), tier, (index - tier * (tier - 1) * 3 - 1) / (double) tier);
+//                            index, StarUtils.realIndexOf(pos.x, pos.y), tier, (index - tier * (tier - 1) * 3 - 1) / (double) tier / 6);
                 }
             }
         }
         System.out.printf("[totalNumber]: %d, [ErrorNumber]: %d%n", testTier*(testTier+1)*3*6, error);
+
+//        for (int index = 0; index < testTier * (testTier+1) * 3; index++) {
+//            Position pos = StarUtils.positionOf(index);
+//            int tier = StarUtils.tierOf(index);
+//            int t = StarUtils.realIndexOf(pos.x, pos.y);
+//            if (tier != t)
+//                if (tier != t + 1)
+//                    error++;
+//        }
+//        System.out.printf("[totalNumber]: %d, [ErrorNumber]: %d%n", testTier*(testTier+1)*3*6, error);
     }
 
     @Test
@@ -72,7 +81,7 @@ public class StarUtilsTest {
         Position pos = StarUtils.positionOf(0);
         for (int i = 0; i < 6; i++) {
             var v = new Vector2(pos.x, pos.y)
-                    .add(new Vector2(0.5, 0).rotate(i * Math.PI / 3));
+                    .add(new Vector2(0, 0).rotate(i * Math.PI / 3));
             Assertions.assertEquals(StarUtils.realIndexOf(v.x, v.y), 0);
         }
 
@@ -88,9 +97,9 @@ public class StarUtilsTest {
 
     @Test
     public void Test_singleBlock() {
-        int index = 1;
+        int index = 6889;
         Position pos = StarUtils.positionOf(index);
-        pos.y -= 0.49;
+//        pos.y -= 0.49;
         System.out.printf("[index]: %d, [tier]: %d, [x]: %f, [y]: %f, [angle]: %f%n",
                 index, StarUtils.tierOf(index), pos.x, pos.y, (Math.atan2(pos.y, pos.x) + Math.PI * 2) % (Math.PI * 2));
         System.out.printf("Position at [%d] is : [%d]%n", index, StarUtils.realIndexOf(pos.x, pos.y));
