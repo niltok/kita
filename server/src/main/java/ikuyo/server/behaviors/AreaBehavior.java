11fdfbfecdb7155b9ba8d7967518ce38ca9caee4
niltok <huohuomoe@outlook.com>
2023-05-12 14:48:07 +0800

    reduce work set

--- server/src/main/java/ikuyo/server/behaviors/AreaBehavior.java
+++ server/src/main/java/ikuyo/server/behaviors/AreaBehavior.java
@@ -5,6 +5,7 @@ import ikuyo.api.behaviors.Behavior;
 import ikuyo.api.datatypes.Drawable;
 import ikuyo.server.api.AreaState;
 import ikuyo.server.api.CommonContext;
+import ikuyo.server.api.UpdatedContext;
 import ikuyo.utils.DataStatic;
 import ikuyo.utils.MsgDiffer;
 import ikuyo.utils.StarUtils;
@@ -27,8 +28,64 @@ public class AreaBehavior implements Behavior<CommonContext> {
                 writeCache(context, state, blocks);
             });
         }
-        var loadList = new ConcurrentSkipListSet<Integer>();
+        lazyLoad(context, updated);
+        if (AreaState.workSet) {
+            updateWorkSet(context, updated);
+        }
+        if (context.writeBackLock.compareAndExchange(false, true)) return;
+        updated.areas.stream().parallel().forEach(area -> {
+            writeCache(context, context.areaStates.get(area), StarUtils.getBlocksAt(area));
+        });
+        updated.areas = new HashSet<>();
+        context.writeBackLock.set(false);
+    }
+
+    private static void updateWorkSet(CommonContext context, UpdatedContext updated) {
         var enableList = new ConcurrentSkipListSet<Integer>();
+        updated.users().stream().parallel().forEach(id -> {
+            var info = context.getInfo(id);
+            if (info == null || !info.online) return;
+            StarUtils.areasAround(info.x, info.y, StarUtils.areaSize * 2 + 1).forEach(area -> {
+                enableList.add(area);
+            });
+        });
+        var engine = context.engine();
+        engine.bullets.values().stream().parallel().forEach(bullet -> {
+            if (bullet == null) return;
+            var pos = bullet.getBody().getWorldCenter();
+            StarUtils.areasAround(pos.x, pos.y, StarUtils.areaSize * 2 + 1).forEach(area -> {
+                enableList.add(area);
+            });
+        });
+        var delta = new AtomicInteger(0);
+        enableList.stream().parallel().forEach(area -> {
+            if (context.enabledAreas.contains(area)) return;
+            delta.incrementAndGet();
+            StarUtils.getBlocksAt(area).forEach(id -> {
+                var block = context.star().starInfo().blocks[id];
+                if (block.isSurface && block.isCollisible)
+                    engine.enableBody(engine.surfaceBlocks.get(id));
+            });
+            var state = context.areaStates.get(area);
+            state.enabled = true;
+        });
+        context.enabledAreas.stream().parallel().forEach(area -> {
+            if (enableList.contains(area)) return;
+            delta.incrementAndGet();
+            StarUtils.getBlocksAt(area).forEach(id -> {
+                var block = context.star().starInfo().blocks[id];
+                if (block.isSurface && block.isCollisible)
+                    engine.disableBody(engine.surfaceBlocks.get(id));
+            });
+            var state = context.areaStates.get(area);
+            state.enabled = false;
+        });
+        context.areaDelta = delta.getAcquire();
+        context.enabledAreas = enableList;
+    }
+
+    private static void lazyLoad(CommonContext context, UpdatedContext updated) {
+        var loadList = new ConcurrentSkipListSet<Integer>();
         updated.users().stream().parallel().forEach(id -> {
             var info = context.getInfo(id);
             if (info == null || !info.online) return;
@@ -40,54 +97,12 @@ public class AreaBehavior implements Behavior<CommonContext> {
                     state.loaded = true;
                     loadList.add(area);
                 }
-                if (AreaState.workSet) enableList.add(area);
             });
         });
         loadList.forEach(area -> {
             var blocks = StarUtils.getBlocksAt(area);
             updated.blocks().addAll(blocks);
         });
-        if (AreaState.workSet) {
-            var engine = context.engine();
-            engine.bullets.values().stream().parallel().forEach(bullet -> {
-                if (bullet == null) return;
-                var pos = bullet.getBody().getWorldCenter();
-                StarUtils.areasAround(pos.x, pos.y, StarUtils.areaSize * 2 + 1).forEach(area -> {
-                    enableList.add(area);
-                });
-            });
-            var delta = new AtomicInteger(0);
-            enableList.stream().parallel().forEach(area -> {
-                if (context.enabledAreas.contains(area)) return;
-                delta.incrementAndGet();
-                StarUtils.getBlocksAt(area).forEach(id -> {
-                    var block = context.star().starInfo().blocks[id];
-                    if (block.isSurface && block.isCollisible)
-                        engine.enableBody(engine.surfaceBlocks.get(id));
-                });
-                var state = context.areaStates.get(area);
-                state.enabled = true;
-            });
-            context.enabledAreas.stream().parallel().forEach(area -> {
-                if (enableList.contains(area)) return;
-                delta.incrementAndGet();
-                StarUtils.getBlocksAt(area).forEach(id -> {
-                    var block = context.star().starInfo().blocks[id];
-                    if (block.isSurface && block.isCollisible)
-                        engine.disableBody(engine.surfaceBlocks.get(id));
-                });
-                var state = context.areaStates.get(area);
-                state.enabled = false;
-            });
-            context.areaDelta = delta.getAcquire();
-            context.enabledAreas = enableList;
-        }
-        if (context.writeBackLock.compareAndExchange(false, true)) return;
-        updated.areas.stream().parallel().forEach(area -> {
-            writeCache(context, context.areaStates.get(area), StarUtils.getBlocksAt(area));
-        });
-        updated.areas = new HashSet<>();
-        context.writeBackLock.set(false);
     }
 
     private static void writeCache(CommonContext context, AreaState state, List<Integer> blocks) {
